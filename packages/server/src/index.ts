1c04eb6d900f4a0b1f9c4745934a3175ceaf5403
Sarfraz <alamsarfraz422@gmail.com>
2022-07-27 17:05:46 +0530

    Cors origin

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://fflow-kohl.vercel.app/"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
